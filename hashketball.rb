f24a938e76d2739c759f660265b48da7a8e91249
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 22:11:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,7 +21,7 @@ end
   }
 
   let(:reggie_evans) {
-    { :player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 }
+    
   }
 
   let(:brook_lopez) {
