dbda9ce383ebfdcf6415ba3c3d5b8d97d21cfc76
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 22:13:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,7 +24,7 @@ end
 
 
   let(:jeff_adrien) {
-    { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 }
+    
   }
 
   let(:bismack_biyombo) {
