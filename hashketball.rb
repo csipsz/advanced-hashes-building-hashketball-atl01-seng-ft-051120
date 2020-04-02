b37ebd830b7cde1d7c4f7270aa8667afe453af92
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 22:12:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,7 +26,7 @@ end
   }
 
   let(:brook_lopez) {
-    { :player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15 }
+    
   }
 
   let(:mason_plumlee) {
