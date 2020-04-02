925b829fdd8c60e229fd4626c5ab2742dc85de57
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 21:41:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,10 +17,7 @@ end
 
 
 
-
-{:player_name => "Brook Lopez" , :number =>11 , :shoe =>17 , :points =>17 , :rebounds => 19, :assists =>10 , :steals =>3 , :blocks =>1 , :slam_dunks =>15 }
-
-{:player_name => "Mason Plumlee", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+{:player_name => "Mason Plumlee", :number =>1 , :shoe =>19 , :points =>26 , :rebounds => 11, :assists =>6 , :steals =>3 , :blocks =>8 , :slam_dunks =>5 }
 
 {:player_name => "Jason Terry" , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
