06ca5d61f5ac49a9b27709524e0e9f6f90e2bc3a
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 21:37:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
 end
 
 
-{:player_name => "Alan Anderson" , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+{:player_name => "Alan Anderson" , :number => 0 , :shoe => 16, :points => 22 , :rebounds => 12 , :assists => 12 , :steals => 3 , :blocks => 1 , :slam_dunks => 1}
 
 {:player_name => "Reggie Evans ", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
