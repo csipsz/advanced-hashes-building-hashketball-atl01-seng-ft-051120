f377b199138d9c2b080306fbe512bfae95cfe1cb
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 21:35:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
 end
 
 
-{:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+{:player_name => "" , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
 {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
