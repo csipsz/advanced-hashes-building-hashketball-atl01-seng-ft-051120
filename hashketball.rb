8444a90cc150e63fa0084bb7c915dc26d6c46f2c
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-02 21:36:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,8 +18,8 @@ end
 
 {:player_name => "Reggie Evans ", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
-{:player_name => "" , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+{:player_name => "Brook Lopez" , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
-{:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+{:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
 
 {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
